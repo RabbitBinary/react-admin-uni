65c20526a71549e4c3fd90a466f6d238a00e2a7b
RabbitBinary <vladimir.labat@gmail.com>
2022-08-17 18:40:26 +0200

    Sidebar

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,87 @@
+import './sidebar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleIcon from '@mui/icons-material/People';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className="top">
+        <span className='logo'>cpanel</span>
+        </div>
+        <hr/>
+      <div className="center">
+        <ul>
+          <p className="title">MAIN</p>
+          <li>
+            <DashboardIcon className='icon'/>
+            <span>Dashboard</span>
+          </li>
+          <p className="title">LISTS</p>
+          <li>
+            <PeopleIcon className='icon'/>
+            <span>Users</span>
+          </li>
+          <li>
+            <StoreIcon className='icon'/>
+            <span>Products</span>
+          </li>
+          <li>
+            <CreditCardIcon className='icon'/>
+            <span>Orders</span>
+          </li>
+          <li>
+            <LocalShippingIcon className='icon'/>
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFULL</p>
+          <li>
+            <QueryStatsIcon className='icon'/>
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsIcon className='icon'/>
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <MonitorHeartIcon className='icon'/>
+            <span>System Health</span>
+          </li>
+          <li>
+            <AssignmentIcon className='icon'/>
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsIcon className='icon'/>
+            <span>Setings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <AccountBoxIcon className='icon'/>
+            <span>Profile</span>
+          </li>
+          <li>
+            <MeetingRoomIcon className='icon'/>
+            <span>Logout</span>
+          </li>
+        </ul>
+      </div>
+      <div className="bottom">
+        <div className="colorOption"></div>
+        <div className="colorOption"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar
