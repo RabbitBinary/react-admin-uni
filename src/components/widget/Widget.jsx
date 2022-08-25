05467bcf7c0fd9cc00ec4eb9a77d0fd7e1a4a10e
RabbitBinary <vladimir.labat@gmail.com>
2022-08-25 13:04:06 +0200

    Commit#3

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,21 +1,101 @@
 import './widget.scss';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Person from '@mui/icons-material/Person';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 
-const Widget = () => {
+const Widget = ({ type }) => {
+  let data;
+
+  //temporary
+  const amount = 100;
+  const diff = 100;
+
+    switch(type){
+      case "user":
+        data={
+          title:"USERS",
+          isMoney: false,
+          link:"See all users",
+          icon: (
+            <Person 
+              className="icon"
+              style={{
+              color:"crimson",
+              backgroundColor: "rgba(255,0,0,0.2)",
+            }}
+          />
+          ),
+        };
+        break;
+        case "order":
+        data={
+          title:"ORDERS",
+          isMoney: false,
+          link:"See all orders",
+          icon: (
+          <ShoppingCartIcon 
+            className="icon"
+            style={{
+              backgroundColor: "rgba(218,165,32,0.2)",
+              color: "goldenrod",
+            }} 
+            />
+          ),
+        };
+        break;
+        case "earning":
+        data={
+          title:"EARNINGS",
+          isMoney: false,
+          link:"See net earnings",
+          icon: (
+          <MonetizationOnIcon 
+            className="icon" 
+            style={{
+            color:"green",
+            backgroundColor: "rgba(0,128,0,0.2)",
+          }}
+          />
+          ),
+        };
+        break;
+        case "balance":
+        data={
+          title:"BALANCE",
+          isMoney: false,
+          link:"See details",
+          icon: (
+          <AccountBalanceWalletIcon 
+            className="icon" 
+            style={{
+            color:"purple",
+            backgroundColor: "rgba(128,0,128,0.2)",
+          }}
+          />
+          ),
+        };
+        break;
+      default:
+        break;
+    }
+
   return (
     <div className="widget">
       <div className="left">
-        <span className="title">USERS</span>
-        <span className="counter">32152</span>
-        <span className="link">See all user</span>
+        <span className="title">{data.title}</span>
+        <span className="counter">
+          {data.isMoney && "$"} {amount}
+        </span>
+        <span className="link">{data.link}</span>
       </div>
       <div className="right">
-        <div className="pecentage">
+        <div className="percentage positive">
           <KeyboardArrowUpIcon/>
-          20%
+          {diff}%
         </div>
-         <Person/>
+         {data.icon}
       </div>
     </div>
   )
