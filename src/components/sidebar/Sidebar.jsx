5b87109b2fa02612759b7243eb9fca0b1e526f01
RabbitBinary <vladimir.labat@gmail.com>
2022-08-30 15:59:28 +0200

    Commit#11

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,77 +1,84 @@
-import './sidebar.scss';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PeopleIcon from '@mui/icons-material/People';
-import StoreIcon from '@mui/icons-material/Store';
-import CreditCardIcon from '@mui/icons-material/CreditCard';
-import LocalShippingIcon from '@mui/icons-material/LocalShipping';
-import QueryStatsIcon from '@mui/icons-material/QueryStats';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import MonitorHeartIcon from '@mui/icons-material/MonitorHeart';
-import AssignmentIcon from '@mui/icons-material/Assignment';
-import SettingsIcon from '@mui/icons-material/Settings';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import "./sidebar.scss";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import PeopleIcon from "@mui/icons-material/People";
+import StoreIcon from "@mui/icons-material/Store";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import QueryStatsIcon from "@mui/icons-material/QueryStats";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import MonitorHeartIcon from "@mui/icons-material/MonitorHeart";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
+    <div className="sidebar">
       <div className="top">
-        <span className='logo'>c-panel</span>
-        </div>
-        <hr/>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">c-panel</span>
+        </Link>
+      </div>
+      <hr />
       <div className="center">
         <ul>
           <p className="title">MAIN</p>
           <li>
-            <DashboardIcon className='icon'/>
+            <DashboardIcon className="icon" />
             <span>Dashboard</span>
           </li>
           <p className="title">LISTS</p>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PeopleIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <StoreIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
           <li>
-            <PeopleIcon className='icon'/>
-            <span>Users</span>
-          </li>
-          <li>
-            <StoreIcon className='icon'/>
-            <span>Products</span>
-          </li>
-          <li>
-            <CreditCardIcon className='icon'/>
+            <CreditCardIcon className="icon" />
             <span>Orders</span>
           </li>
           <li>
-            <LocalShippingIcon className='icon'/>
+            <LocalShippingIcon className="icon" />
             <span>Delivery</span>
           </li>
           <p className="title">USEFULL</p>
           <li>
-            <QueryStatsIcon className='icon'/>
+            <QueryStatsIcon className="icon" />
             <span>Stats</span>
           </li>
           <li>
-            <NotificationsIcon className='icon'/>
+            <NotificationsIcon className="icon" />
             <span>Notifications</span>
           </li>
           <p className="title">SERVICE</p>
           <li>
-            <MonitorHeartIcon className='icon'/>
+            <MonitorHeartIcon className="icon" />
             <span>System Health</span>
           </li>
           <li>
-            <AssignmentIcon className='icon'/>
+            <AssignmentIcon className="icon" />
             <span>Logs</span>
           </li>
           <li>
-            <SettingsIcon className='icon'/>
+            <SettingsIcon className="icon" />
             <span>Setings</span>
           </li>
           <p className="title">USER</p>
           <li>
-            <AccountBoxIcon className='icon'/>
+            <AccountBoxIcon className="icon" />
             <span>Profile</span>
           </li>
           <li>
-            <MeetingRoomIcon className='icon'/>
+            <MeetingRoomIcon className="icon" />
             <span>Logout</span>
           </li>
         </ul>
@@ -84,4 +91,4 @@ const Sidebar = () => {
   );
 };
 
-export default Sidebar
+export default Sidebar;
